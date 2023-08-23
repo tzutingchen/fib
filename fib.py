0fc1adc70d1d0f0f241be86c731555fb138045c4
tzutinhchen <pingpingting326@gmai.com>
2023-08-23 10:59:04 -0400

    made cofe faster

--- fib.py
+++ fib.py
@@ -3,8 +3,8 @@
 def fib(n):
   if n == 0 or n == 1:
      return n
-  else
+  else:
     return fib(n-1)+fib(n-2)
 
- if __name__=="__main__":
-   print("The 20th Fibonacci Number is".fib(20))
+if __name__ == "__main__":
+  print("The 20th Fibonacci Number is: ", fib(20))
